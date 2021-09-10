bd93fe741ccae2d5094f71286b4532d32b7c2aca
lauren-dowling-designer <laurenj.dowling@gmail.com>
2021-09-10 17:06:39 +0100

    First page

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,23 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/what_service_answer', function (req, res) {
+
+    // Make a variable and give it the value from 'how-many-balls'
+    var whatService = req.session.data['what_service']
+  
+    // Check whether the variable matches a condition
+    if (whatService == "request-title-information"){
+      // Send user to next page
+      res.redirect('/requestTitleInformation/LD_prototype/request_title_information/Request-title-information-1-1')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/requestTitleInformation/LD_prototype/bulk_request/Official-searches-2-32')
+    }
+  
+  })
+
+
 module.exports = router
